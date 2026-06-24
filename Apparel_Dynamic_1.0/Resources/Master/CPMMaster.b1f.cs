719dba1c11804305e14a41f6c89e1c55b7206365
Asif Iftekher Fahim <asififtekherfahim@gmail.com>
2026-06-24 11:10:54 +0600

    dev: new form creation for cpm

---
+++ Apparel_Dynamic_1.0/Resources/Master/CPMMaster.b1f.cs
@@ -0,0 +1,30 @@
+﻿using SAPbouiCOM.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Apparel_Dynamic_1._0.Resources.Master
+{
+    [FormAttribute("Apparel_Dynamic_1._0.Resources.Master.CPMMaster", "Resources/Master/CPMMaster.b1f")]
+    class CPMMaster : UserFormBase
+    {
+        public CPMMaster()
+        {
+        }
+
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+        }
+    }
+}

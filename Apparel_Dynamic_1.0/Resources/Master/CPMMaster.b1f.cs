d3acb340d45536b613e8742decda52d0bf58a57b
Asif Iftekher Fahim <asififtekherfahim@gmail.com>
2026-06-28 12:09:08 +0600

    dev: after basic design of CPM done

--- Apparel_Dynamic_1.0/Resources/Master/CPMMaster.b1f.cs
+++ Apparel_Dynamic_1.0/Resources/Master/CPMMaster.b1f.cs
@@ -18,6 +18,33 @@ namespace Apparel_Dynamic_1._0.Resources.Master
         /// </summary>
         public override void OnInitializeComponent()
         {
+            this.StaticText0 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_0").Specific));
+            this.StaticText1 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_1").Specific));
+            this.StaticText2 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_2").Specific));
+            this.StaticText3 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_3").Specific));
+            this.StaticText4 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_4").Specific));
+            this.StaticText5 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_5").Specific));
+            this.EditText0 = ((SAPbouiCOM.EditText)(this.GetItem("Item_6").Specific));
+            this.EditText1 = ((SAPbouiCOM.EditText)(this.GetItem("Item_7").Specific));
+            this.EditText2 = ((SAPbouiCOM.EditText)(this.GetItem("Item_8").Specific));
+            this.EditText3 = ((SAPbouiCOM.EditText)(this.GetItem("Item_9").Specific));
+            this.EditText4 = ((SAPbouiCOM.EditText)(this.GetItem("Item_10").Specific));
+            this.EditText5 = ((SAPbouiCOM.EditText)(this.GetItem("Item_11").Specific));
+            this.EditText6 = ((SAPbouiCOM.EditText)(this.GetItem("Item_12").Specific));
+            this.ComboBox0 = ((SAPbouiCOM.ComboBox)(this.GetItem("Item_13").Specific));
+            this.EditText7 = ((SAPbouiCOM.EditText)(this.GetItem("Item_14").Specific));
+            this.EditText8 = ((SAPbouiCOM.EditText)(this.GetItem("Item_15").Specific));
+            this.EditText9 = ((SAPbouiCOM.EditText)(this.GetItem("Item_16").Specific));
+            this.Folder0 = ((SAPbouiCOM.Folder)(this.GetItem("Item_18").Specific));
+            this.Button0 = ((SAPbouiCOM.Button)(this.GetItem("Item_19").Specific));
+            this.Button1 = ((SAPbouiCOM.Button)(this.GetItem("Item_20").Specific));
+            this.Folder1 = ((SAPbouiCOM.Folder)(this.GetItem("Item_21").Specific));
+            this.Matrix0 = ((SAPbouiCOM.Matrix)(this.GetItem("Item_22").Specific));
+            this.Button2 = ((SAPbouiCOM.Button)(this.GetItem("Item_23").Specific));
+            this.Button3 = ((SAPbouiCOM.Button)(this.GetItem("Item_24").Specific));
+            this.Matrix1 = ((SAPbouiCOM.Matrix)(this.GetItem("Item_25").Specific));
+            this.OnCustomInitialize();
+
         }
 
         /// <summary>
@@ -26,5 +53,37 @@ namespace Apparel_Dynamic_1._0.Resources.Master
         public override void OnInitializeFormEvents()
         {
         }
+
+        private SAPbouiCOM.StaticText StaticText0;
+
+        private void OnCustomInitialize()
+        {
+
+        }
+
+        private SAPbouiCOM.StaticText StaticText1;
+        private SAPbouiCOM.StaticText StaticText2;
+        private SAPbouiCOM.StaticText StaticText3;
+        private SAPbouiCOM.StaticText StaticText4;
+        private SAPbouiCOM.StaticText StaticText5;
+        private SAPbouiCOM.EditText EditText0;
+        private SAPbouiCOM.EditText EditText1;
+        private SAPbouiCOM.EditText EditText2;
+        private SAPbouiCOM.EditText EditText3;
+        private SAPbouiCOM.EditText EditText4;
+        private SAPbouiCOM.EditText EditText5;
+        private SAPbouiCOM.EditText EditText6;
+        private SAPbouiCOM.ComboBox ComboBox0;
+        private SAPbouiCOM.EditText EditText7;
+        private SAPbouiCOM.EditText EditText8;
+        private SAPbouiCOM.EditText EditText9;
+        private SAPbouiCOM.Folder Folder0;
+        private SAPbouiCOM.Button Button0;
+        private SAPbouiCOM.Button Button1;
+        private SAPbouiCOM.Folder Folder1;
+        private SAPbouiCOM.Matrix Matrix0;
+        private SAPbouiCOM.Button Button2;
+        private SAPbouiCOM.Button Button3;
+        private SAPbouiCOM.Matrix Matrix1;
     }
 }

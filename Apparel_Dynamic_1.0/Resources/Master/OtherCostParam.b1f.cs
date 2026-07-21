c34cf51d55e50bb43b8f26a38a42a562d1d20fc9
Asif Iftekher Fahim <asififtekherfahim@gmail.com>
2026-07-21 14:30:07 +0600

    Dev: Design done for Other Cost Param

--- Apparel_Dynamic_1.0/Resources/Master/OtherCostParam.b1f.cs
+++ Apparel_Dynamic_1.0/Resources/Master/OtherCostParam.b1f.cs
@@ -18,6 +18,16 @@ namespace Apparel_Dynamic_1._0.Resources.Master
         /// </summary>
         public override void OnInitializeComponent()
         {
+            this.StaticText0 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_0").Specific));
+            this.StaticText1 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_1").Specific));
+            this.EditText0 = ((SAPbouiCOM.EditText)(this.GetItem("Item_2").Specific));
+            this.EditText1 = ((SAPbouiCOM.EditText)(this.GetItem("Item_3").Specific));
+            this.EditText2 = ((SAPbouiCOM.EditText)(this.GetItem("Item_4").Specific));
+            this.Matrix0 = ((SAPbouiCOM.Matrix)(this.GetItem("Item_5").Specific));
+            this.Button0 = ((SAPbouiCOM.Button)(this.GetItem("Item_6").Specific));
+            this.Button1 = ((SAPbouiCOM.Button)(this.GetItem("Item_7").Specific));
+            this.OnCustomInitialize();
+
         }
 
         /// <summary>
@@ -26,5 +36,20 @@ namespace Apparel_Dynamic_1._0.Resources.Master
         public override void OnInitializeFormEvents()
         {
         }
+
+        private SAPbouiCOM.StaticText StaticText0;
+
+        private void OnCustomInitialize()
+        {
+
+        }
+
+        private SAPbouiCOM.StaticText StaticText1;
+        private SAPbouiCOM.EditText EditText0;
+        private SAPbouiCOM.EditText EditText1;
+        private SAPbouiCOM.EditText EditText2;
+        private SAPbouiCOM.Matrix Matrix0;
+        private SAPbouiCOM.Button Button0;
+        private SAPbouiCOM.Button Button1;
     }
 }

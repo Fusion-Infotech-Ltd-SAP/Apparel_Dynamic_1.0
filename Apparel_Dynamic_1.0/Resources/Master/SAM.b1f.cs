d500b4c4289d54d83ed5c1ac8a4133504bbae8fa
Asif Iftekher Fahim <asififtekherfahim@gmail.com>
2026-07-26 15:40:59 +0600

    Dev: Design the SAM Form

---
+++ Apparel_Dynamic_1.0/Resources/Master/SAM.b1f.cs
@@ -0,0 +1,61 @@
+﻿using SAPbouiCOM.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Apparel_Dynamic_1._0.Resources.Master
+{
+    [FormAttribute("Apparel_Dynamic_1._0.Resources.Master.SAM", "Resources/Master/SAM.b1f")]
+    class SAM : UserFormBase
+    {
+        public SAM()
+        {
+        }
+
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+            this.StaticText0 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_0").Specific));
+            this.StaticText1 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_1").Specific));
+            this.EditText0 = ((SAPbouiCOM.EditText)(this.GetItem("Item_2").Specific));
+            this.EditText1 = ((SAPbouiCOM.EditText)(this.GetItem("Item_3").Specific));
+            this.EditText2 = ((SAPbouiCOM.EditText)(this.GetItem("Item_4").Specific));
+            this.Folder0 = ((SAPbouiCOM.Folder)(this.GetItem("Item_6").Specific));
+            this.Folder1 = ((SAPbouiCOM.Folder)(this.GetItem("Item_7").Specific));
+            this.Button0 = ((SAPbouiCOM.Button)(this.GetItem("Item_8").Specific));
+            this.Button1 = ((SAPbouiCOM.Button)(this.GetItem("Item_9").Specific));
+            this.Matrix0 = ((SAPbouiCOM.Matrix)(this.GetItem("Item_10").Specific));
+            this.Grid0 = ((SAPbouiCOM.Grid)(this.GetItem("Item_11").Specific));
+            this.OnCustomInitialize();
+
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+        }
+
+        private SAPbouiCOM.StaticText StaticText0;
+
+        private void OnCustomInitialize()
+        {
+
+        }
+
+        private SAPbouiCOM.StaticText StaticText1;
+        private SAPbouiCOM.EditText EditText0;
+        private SAPbouiCOM.EditText EditText1;
+        private SAPbouiCOM.EditText EditText2;
+        private SAPbouiCOM.Folder Folder0;
+        private SAPbouiCOM.Folder Folder1;
+        private SAPbouiCOM.Button Button0;
+        private SAPbouiCOM.Button Button1;
+        private SAPbouiCOM.Matrix Matrix0;
+        private SAPbouiCOM.Grid Grid0;
+    }
+}

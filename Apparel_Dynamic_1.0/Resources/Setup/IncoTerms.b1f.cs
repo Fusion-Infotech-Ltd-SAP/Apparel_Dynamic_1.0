cf141cc963314e9a60a5f0ca1dfb3e60ce00e8e6
Asif Iftekher Fahim <asififtekherfahim@gmail.com>
2026-07-26 12:15:29 +0600

    Dev : IncoTerm Form Creation and Design Done

---
+++ Apparel_Dynamic_1.0/Resources/Setup/IncoTerms.b1f.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SAPbouiCOM.Framework;
+
+namespace Apparel_Dynamic_1._0.Resources.Setup
+{
+    [FormAttribute("Apparel_Dynamic_1._0.Resources.Setup.IncoTerms", "Resources/Setup/IncoTerms.b1f")]
+    class IncoTerms : UserFormBase
+    {
+        public IncoTerms()
+        {
+        }
+
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+        }
+    }
+}

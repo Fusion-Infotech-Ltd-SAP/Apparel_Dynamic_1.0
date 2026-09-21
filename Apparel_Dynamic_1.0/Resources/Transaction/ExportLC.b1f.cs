ee33f0671e95e3baa8aedc52b3855690f6dc727d
Asif Iftekher Fahim <asififtekherfahim@gmail.com>
2026-09-21 11:46:42 +0600

    dev : Link Button Add

--- Apparel_Dynamic_1.0/Resources/Transaction/ExportLC.b1f.cs
+++ Apparel_Dynamic_1.0/Resources/Transaction/ExportLC.b1f.cs
@@ -17,19 +17,20 @@ namespace Apparel_Dynamic_1._0.Resources.Transaction
 
         private SAPbouiCOM.ComboBox CBSTATMR, CBSTATCM, CBCOMPNY, CBSERIES, CBINTRMS, CBPYTRMS, CBLCTRMS;
 
-        private SAPbouiCOM.EditText CBCUSTMR, ETSCNO, ETLCNO, ETDOCTRY, ETDOCNUM, ETLCDESC, ETCURR, ETBP1BNK, ETBP2BNK, ETHUSBNK, ETLCVAL, ETDOCDAT, ETISUDAT, ETSHPDAT, ETEXPDAT, ETB2BPER, ETB2BAMT, ETAMDNO;
+        private SAPbouiCOM.EditText ETCUSTMR, ETSCNO, ETLCNO, ETDOCTRY, ETDOCNUM, ETLCDESC, ETCURR, ETBP1BNK, ETBP2BNK, ETHUSBNK, ETLCVAL, ETDOCDAT, ETISUDAT, ETSHPDAT, ETEXPDAT, ETB2BPER, ETB2BAMT, ETAMDNO;
 
         private SAPbouiCOM.Folder TABSODR, TABAMDTL, TABATTCH;
 
         private SAPbouiCOM.Matrix MTXSLODR, MTXATTAC;
 
-        private SAPbouiCOM.Button BRWSBTN, DISPBTN, DELBTN, BTNOK, BTNCANCEL, BTNLDATA, BTNAMND;
+        private SAPbouiCOM.Button BRWSBTN, DISPBTN, DELBTN, ADDButton, CancelButton, BTNLDATA, BTNAMND;
 
         private SAPbouiCOM.Grid GRDAMDTL;
 
+        private SAPbouiCOM.LinkedButton LKSCNO, LKCUSTMR;
+
         public override void OnInitializeComponent()
         {
-
             this.STSTATMR = ((SAPbouiCOM.StaticText)(this.GetItem("STSTATMR").Specific));
             this.STSTATCM = ((SAPbouiCOM.StaticText)(this.GetItem("STSTATCM").Specific));
             this.STCOMPNY = ((SAPbouiCOM.StaticText)(this.GetItem("STCOMPNY").Specific));
@@ -62,7 +63,7 @@ namespace Apparel_Dynamic_1._0.Resources.Transaction
             this.CBPYTRMS = ((SAPbouiCOM.ComboBox)(this.GetItem("CBPYTRMS").Specific));
             this.CBLCTRMS = ((SAPbouiCOM.ComboBox)(this.GetItem("CBLCTRMS").Specific));
 
-            this.CBCUSTMR = ((SAPbouiCOM.EditText)(this.GetItem("CBCUSTMR").Specific));
+            this.ETCUSTMR = ((SAPbouiCOM.EditText)(this.GetItem("ETCUSTMR").Specific));
             this.ETSCNO = ((SAPbouiCOM.EditText)(this.GetItem("ETSCNO").Specific));
             this.ETLCNO = ((SAPbouiCOM.EditText)(this.GetItem("ETLCNO").Specific));
             this.ETDOCTRY = ((SAPbouiCOM.EditText)(this.GetItem("ETDOCTRY").Specific));
@@ -91,13 +92,15 @@ namespace Apparel_Dynamic_1._0.Resources.Transaction
             this.BRWSBTN = ((SAPbouiCOM.Button)(this.GetItem("BRWSBTN").Specific));
             this.DISPBTN = ((SAPbouiCOM.Button)(this.GetItem("DISPBTN").Specific));
             this.DELBTN = ((SAPbouiCOM.Button)(this.GetItem("DELBTN").Specific));
-            this.BTNOK = ((SAPbouiCOM.Button)(this.GetItem("1").Specific));
-            this.BTNCANCEL = ((SAPbouiCOM.Button)(this.GetItem("2").Specific));
-
-            this.GRDAMDTL = ((SAPbouiCOM.Grid)(this.GetItem("GRDAMDTL").Specific));
-
+            this.ADDButton  = ((SAPbouiCOM.Button)(this.GetItem("1").Specific));
+            this.CancelButton = ((SAPbouiCOM.Button)(this.GetItem("2").Specific));
             this.BTNLDATA = ((SAPbouiCOM.Button)(this.GetItem("BTNLDATA").Specific));
             this.BTNAMND = ((SAPbouiCOM.Button)(this.GetItem("BTNAMND").Specific));
+            this.GRDAMDTL = ((SAPbouiCOM.Grid)(this.GetItem("GRDAMDTL").Specific));
+
+
+            this.LKSCNO = ((SAPbouiCOM.LinkedButton)(this.GetItem("LKSCNO").Specific));
+            this.LKCUSTMR = ((SAPbouiCOM.LinkedButton)(this.GetItem("LKCUSTMR").Specific));
             this.OnCustomInitialize();
 
         }
@@ -116,65 +119,8 @@ namespace Apparel_Dynamic_1._0.Resources.Transaction
 
         }
 
-        private SAPbouiCOM.StaticText StaticText1;
-        private SAPbouiCOM.StaticText StaticText2;
-        private SAPbouiCOM.StaticText StaticText3;
-        private SAPbouiCOM.StaticText StaticText4;
-        private SAPbouiCOM.StaticText StaticText5;
-        private SAPbouiCOM.StaticText StaticText6;
-        private SAPbouiCOM.StaticText StaticText7;
-        private SAPbouiCOM.StaticText StaticText8;
-        private SAPbouiCOM.StaticText StaticText9;
-        private SAPbouiCOM.StaticText StaticText10;
-        private SAPbouiCOM.StaticText StaticText11;
-        private SAPbouiCOM.StaticText StaticText12;
-        private SAPbouiCOM.StaticText StaticText13;
-        private SAPbouiCOM.StaticText StaticText14;
-        private SAPbouiCOM.StaticText StaticText15;
-        private SAPbouiCOM.StaticText StaticText16;
-        private SAPbouiCOM.StaticText StaticText17;
-        private SAPbouiCOM.StaticText StaticText18;
-        private SAPbouiCOM.StaticText StaticText19;
-        private SAPbouiCOM.StaticText StaticText20;
-        private SAPbouiCOM.StaticText StaticText21;
-        private SAPbouiCOM.StaticText StaticText22;
-        private SAPbouiCOM.ComboBox ComboBox0;
-        private SAPbouiCOM.ComboBox ComboBox1;
-        private SAPbouiCOM.ComboBox ComboBox2;
-        private SAPbouiCOM.ComboBox ComboBox3;
-        private SAPbouiCOM.ComboBox ComboBox4;
-        private SAPbouiCOM.ComboBox ComboBox5;
-        private SAPbouiCOM.EditText EditText0;
-        private SAPbouiCOM.EditText EditText1;
-        private SAPbouiCOM.EditText EditText2;
-        private SAPbouiCOM.EditText EditText3;
-        private SAPbouiCOM.EditText EditText4;
-        private SAPbouiCOM.EditText EditText5;
-        private SAPbouiCOM.EditText EditText6;
-        private SAPbouiCOM.EditText EditText7;
-        private SAPbouiCOM.EditText EditText8;
-        private SAPbouiCOM.EditText EditText9;
-        private SAPbouiCOM.EditText EditText10;
-        private SAPbouiCOM.ComboBox ComboBox6;
-        private SAPbouiCOM.EditText EditText11;
-        private SAPbouiCOM.EditText EditText12;
-        private SAPbouiCOM.EditText EditText13;
-        private SAPbouiCOM.EditText EditText14;
-        private SAPbouiCOM.EditText EditText15;
-        private SAPbouiCOM.EditText EditText16;
-        private SAPbouiCOM.EditText EditText17;
-        private SAPbouiCOM.Folder Folder0;
-        private SAPbouiCOM.Folder Folder1;
-        private SAPbouiCOM.Folder Folder2;
-        private SAPbouiCOM.Matrix Matrix0;
-        private SAPbouiCOM.Matrix Matrix1;
-        private SAPbouiCOM.Button Button0;
-        private SAPbouiCOM.Button Button1;
-        private SAPbouiCOM.Button Button2;
-        private SAPbouiCOM.Button Button3;
-        private SAPbouiCOM.Button Button4;
-        private SAPbouiCOM.Grid Grid0;
-        private SAPbouiCOM.Button Button5;
-        private SAPbouiCOM.Button Button6;
+       
+      
+
     }
 }

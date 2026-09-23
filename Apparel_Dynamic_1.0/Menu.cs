923c2c44d6134d3bd62e5ed13e8b0a7e1edfec39
Asif Iftekher Fahim <asififtekherfahim@gmail.com>
2026-09-23 17:55:57 +0600

    Dev: Develope this form

--- Apparel_Dynamic_1.0/Menu.cs
+++ Apparel_Dynamic_1.0/Menu.cs
@@ -930,19 +930,24 @@ namespace Apparel_Dynamic_1._0
 
                         return;
                     }
+
+                    SAPbouiCOM.Form oForm = null;
+
                     try
                     {
                         ExportLC activeForm = new ExportLC();
                         activeForm.Show();
 
-                        SAPbouiCOM.Form oForm = (SAPbouiCOM.Form)Application.SBO_Application.Forms.Item("FIL_FRM_EXPLC");
+                        oForm = Application.SBO_Application.Forms.Item("FIL_FRM_EXPLC");
+                        oForm.Freeze(true);
+
                         SAPbouiCOM.Matrix MTXSLODR = (SAPbouiCOM.Matrix)oForm.Items.Item("MTXSLODR").Specific;
                         SAPbouiCOM.Matrix MTXATTAC = (SAPbouiCOM.Matrix)oForm.Items.Item("MTXATTAC").Specific;
                         SAPbouiCOM.Grid GRDAMDTL = (SAPbouiCOM.Grid)oForm.Items.Item("GRDAMDTL").Specific;
 
                         MTXSLODR.AutoResizeColumns();
                         MTXATTAC.AutoResizeColumns();
-                        GRDAMDTL.AutoResizeColumns();
+                        //GRDAMDTL.AutoResizeColumns();
 
                         // Series Initialization
                         if (oForm.Mode == SAPbouiCOM.BoFormMode.fm_ADD_MODE)
@@ -951,18 +956,30 @@ namespace Apparel_Dynamic_1._0
                             Global.GFunc.SetItemsEnabled(oForm, true, "CBSERIES", "ETDOCDAT");
 
                             string today = DateTime.Now.ToString("yyyyMMdd");
-                            SAPbouiCOM.DBDataSource oDBH = oForm.DataSources.DBDataSources.Item("@FIL_DH_OLCM");
+                            SAPbouiCOM.DBDataSource oDBH =oForm.DataSources.DBDataSources.Item("@FIL_DH_OLCM");
                             oDBH.SetValue("U_DOCDATE", 0, today);
-
                             ((SAPbouiCOM.EditText)oForm.Items.Item("ETDOCDAT").Specific).Value = today;
-                            UpdateSeriesAndDocNumByDate(oForm, oDBH, today, "FIL_D_OLCM");
-
+                            UpdateSeriesAndDocNumByDate(oForm,oDBH,today,"FIL_D_OLCM");
                         }
+
+                        // Branch combo
+                        LoadUserBranches(oForm, "CBCOMPNY");
 
                     }
                     catch (Exception ex)
                     {
                         Global.GFunc.ShowError("Failed to open Export form.\n" + ex.Message);
+                    }
+                    finally
+                    {
+                        if (oForm != null)
+                        {
+                            try
+                            {
+                                oForm.Freeze(false);
+                            }
+                            catch { }
+                        }
                     }
                 }
 
